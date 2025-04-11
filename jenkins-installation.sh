2341cc82da53f4a5cfc24161781562f8003dad92
jayachandra_kumar <jayachandrakumar21@gmail.com>
2025-04-11 09:55:35 +0530

    bash script for jenkins installation

---
+++ jenkins-installation.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo curl -o /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+# sudo yum upgrade --> You can skip this, as it takes too much time.
+# Add required dependencies for the Jenkins package
+sudo yum install fontconfig java-17-openjdk -y
+sudo yum install jenkins -y
+sudo systemctl daemon-reload
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
